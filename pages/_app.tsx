9f4538cf0a4535b1bf7baa5d728a9df6740e2b5d
tewarig <gauravtewari111@gmail.com>
2022-04-09 12:23:39 +0530

    feat: chara ui added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import { UserProvider } from "@supabase/supabase-auth-helpers/react";
 import { supabaseClient } from "@supabase/supabase-auth-helpers/nextjs";
 import "./../style.css";
-
-export default function MyApp({ Component, pageProps }) {
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main className={"dark"}>
+    <ChakraProvider>
       <UserProvider supabaseClient={supabaseClient}>
-        <Component {...pageProps} />
+          <Component {...pageProps} />
       </UserProvider>
-    </main>
+    </ChakraProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,33 @@
-import { useUser, Auth } from '@supabase/supabase-auth-helpers/react'
-import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs'
-import { useEffect, useState } from 'react'
+import { useUser, Auth } from "@supabase/supabase-auth-helpers/react";
+import { supabaseClient } from "@supabase/supabase-auth-helpers/nextjs";
+import { useEffect, useState } from "react";
 
 const LoginPage = () => {
-  const { user, error } = useUser()
-  const [data, setData] = useState<any>({})
+  const { user, error } = useUser();
+  const [data, setData] = useState({});
 
   useEffect(() => {
     async function loadData() {
-      const { data } = await supabaseClient.from('test').select('*')
-      setData(data)
+      const { data: any } = await supabaseClient.from("test").select("*");
+      setData(data);
     }
     // Only run query once user is logged in.
-    if (user) loadData()
-  }, [user])
+    if (user) loadData();
+  }, [user]);
 
   if (!user)
     return (
       <>
-        {/* {error && <p>{error.message}</p>} */}
+        {error && <p>{error.message}</p>}
         <Auth
-          onlyThirdPartyProviders={true}
+          onlyThirdPartyProviders={false}
           supabaseClient={supabaseClient}
-          providers={['google', 'github','twitter']}
+          providers={["google", "github", "twitter"]}
           socialLayout="vertical"
           socialButtonSize="xlarge"
         />
       </>
-    )
+    );
 
   return (
     <>
@@ -37,7 +37,7 @@ const LoginPage = () => {
       <p>client-side data fetching with RLS</p>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
