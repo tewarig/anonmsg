30ee9e56aaa9bb9c358ac7f4d98c16984962a664
tewarig <gauravtewari111@gmail.com>
2022-04-12 18:26:24 +0530

    feat: check enabled

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ module.exports = {
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
 };
